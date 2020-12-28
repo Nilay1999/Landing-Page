76c85a2587a9208d1eb0c349a83eaa3be60f0ef9
Nilay911 <64137277+Nilay911@users.noreply.github.com>
2020-12-28 10:57:13 +0530

    changes

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -1,3 +1,13 @@
+/* Active Hover */
+$(document).ready(function() {
+    $('ul.navbar-nav > li')
+        .click(function(e) {
+            $('ul.navbar-nav > li')
+                .removeClass('active');
+            $(this).addClass('active');
+        });
+});
+
 /* 1st Slider */
 $(document).ready(function() {
     $("#owl-demo").owlCarousel({
