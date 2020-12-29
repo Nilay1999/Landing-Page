a8f183f3eea8aff21d5b56f8a6988e8e5018f355
Nilay911 <64137277+Nilay911@users.noreply.github.com>
2020-12-29 14:02:51 +0530

    chngs

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -8,6 +8,49 @@ $(document).ready(function() {
         });
 });
 
+var lastId,
+    topMenu = $("nav"),
+    topMenuHeight = topMenu.outerHeight() + 15,
+
+    menuItems = topMenu.find("a"),
+
+    scrollItems = menuItems.map(function() {
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
+
+menuItems.click(function(e) {
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top - $("nav").outerHeight();
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+});
+
+
+
+$(window).scroll(function() {
+
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    var cur = scrollItems.map(function() {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href='#" + id + "']").parent().addClass("active");
+    }
+});
+
 /* 1st Slider */
 $(document).ready(function() {
     $("#owl-demo").owlCarousel({
